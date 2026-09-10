36866892a24b4573ccfefad2ddcfa414110c684b
allankamuran-photo <allan.kamuran@gmail.com>
2026-09-10 19:37:20 +0200

    Clip internal globes to page headers

---
+++ components/internal-globe.tsx
@@ -0,0 +1,3 @@
+import RotatingEarth from '@/components/ui/wireframe-dotted-globe';
+
+export function InternalGlobe(){return <RotatingEarth width={700} height={700} className="internal-globe-background internal-globe-dark"/>}
